b934a7bfa9f957948199cc281027d2855cb0ffc3
belen desvoignes <belendesvoignes@gmail.com>
2024-03-22 00:28:24 -0300

    Agregue emojis

--- game.py
+++ game.py
@@ -12,11 +12,10 @@ fallos= 0
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
 
-print("----------¡Bienvenido al juego de adivinanzas!----------")
+print("--------😀😀😀😀😀--------¡Bienvenido al juego de adivinanzas!--------😀😀😀😀😀--------")
 print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es")
-print ("Elegí  el nivel de dificultad.   1.Fácil   2.Media   3.Difícil")
+print ("Elegí  el nivel de dificultad.   1.💚💚💚Fácil   2.💛💛💛Media   3.❤️❤️❤️Difícil")
 dificultad = (input ())
-
 if (dificultad == "1"):  #  si la dificultad es 1, agrego las vocales a las letras adivinadas
     guessed_letters.extend (['a','e','i','o','u', 'ó'])
     word_displayed = ""  #var que va a mostrar la palabra parcialmente adivinada 
@@ -35,21 +34,21 @@ else:
 
 
 # Mostrarla palabra parcialmente adivinada
-print(f"Palabra: {word_displayed}")
+print(f"Palabra ▶️ {word_displayed}")
 
 while (fallos != 10):
     # Pedir al jugador que ingrese una letra
     letter = input("Ingresa una letra: ").lower()
     
     if ( letter  == " "): 
-        print ("----------XXX----------")
+        print ("❌❌❌❌❌❌❌❌❌❌❌❌❌❌")
         print(" Error. No has ingresado una letra ")
-        print ("Has sumado 1 fallo")
+        print ("😢😢😢Has sumado 1 fallo😢😢😢")
         fallos+= 1
 
     # Verificar si la letra ya ha sido adivinada
     if letter in guessed_letters:
-        print("Ya has intentado con esa letra. Intenta con otra.")
+        print(" ▶️ Ya has intentado con esa letra. Intenta con otra.")
         continue
 
     # Agregar la letra a la lista de letras adivinadas
@@ -57,8 +56,10 @@ while (fallos != 10):
 
     # Verificar si la letra está en la palabra secreta
     if letter in secret_word:
+        print ("✅✅✅✅✅✅✅✅✅✅✅✅")
         print("¡Bien hecho! La letra está en la palabra.")
     else:
+        print ("❌❌❌❌❌❌❌❌❌❌❌❌")
         print("Lo siento, la letra no está en la palabra.")
         print ("Has sumado 1 fallo")
         fallos+= 1
@@ -72,11 +73,14 @@ while (fallos != 10):
             letters.append("_")
 
     word_displayed = "".join(letters)
-    print(f"Palabra: {word_displayed}")
+    print(f"Palabra ▶️: {word_displayed}")
     # Verificar si se ha adivinado la palabra completa
     if word_displayed == secret_word:
+        print ("😁😁😁😁😁🥰🥰🥰🥰🥰")
         print(f"¡Felicidades! Has adivinado la palabra secreta: {secret_word}")
+        print ("🎈🎈🎈🎈🎊🎊🎊🎊✨✨✨✨")
         break
 else:
+    print ("😭😭😭😭😭😭😭😭😭😭😭😭😭")
     print(f"¡Oh no! Has agotado tus intentos.")
-    print (f"La palabra secreta era :", {secret_word})
+    print (f"La palabra secreta era ▶️:", {secret_word})
